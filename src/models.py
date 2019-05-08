39eacc2073373602e2774b9c9c597771a03192dc
Azamat Omuraliev <azamat.omu@gmail.com>
2019-05-08 13:33:57 +0200

    han minor fix

--- src/models.py
+++ src/models.py
@@ -14,17 +14,18 @@ class BaselineEncoder(nn.Module):
         return output
 
 # model combining word and sentence level attention encoders
-class HierAttNet(nn.Module):
-    def __init__(self, word_input_dim, sent_input_dim, 
+class HierarchicalAttentionNet(nn.Module):
+	# initialize the model
+    def __init__(self, word_input_dim, 
                  word_hidden_dim, sent_hidden_dim,
                  batch_size, num_classes, embedding,
                  max_sent_len, max_word_len):
-        super(HierAttNet, self).__init__()
+        super(HierarchicalAttentionNet, self).__init__()
         self.batch_size = batch_size
-        self.word_hidden_dim = word_hidden_dim
-        self.sent_hidden_dim = sent_hidden_dim
-        self.word_input_dim = word_input_dim
-        self.sent_input_dim = sent_input_dim
+        self.word_hidden_dim = word_hidden_dim 
+        self.sent_hidden_dim = sent_hidden_dim # 
+        self.word_input_dim = word_input_dim # word embedding dimension
+        self.sent_input_dim = 2 * self.word_hidden_dim # sentence input is 2*word_hidden dim (concat of 2 LSTM hidden states)
         self.max_sent_len = max_sent_len
         self.max_word_len = max_word_len
         self.num_classes = num_classes
@@ -50,3 +51,41 @@ class HierAttNet(nn.Module):
         output, self.sent_hidden_state = self.sent_att(output, self.sent_hidden_state)
 
         return output
+
+
+# slightly adapted classifier layer for SNLI from practical 1
+class InferenceClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, classes, encoder, embedding):
+        super(InferenceClassifier, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.classes = len(classes)
+        self.embedding = nn.Embedding.from_pretrained(embedding)
+        self.encoder = WordAttentionRNN(self.input_size, 
+                                        self.hidden_size, 
+                                        self.embedding)
+        self.clf = nn.Sequential(
+                nn.Linear(self.input_size, self.hidden_size), #Input layer
+                nn.Linear(self.hidden_size, self.classes), #Softmax layer
+                )
+
+    def forward(self, premise_batch, hypothesis_batch):
+        pre_s = premise_batch[0]
+        pre_l = premise_batch[1]
+        hyp_s = hypothesis_batch[0]
+        hyp_l = hypothesis_batch[1]
+        u_embed = self.embedding(pre_s)
+        v_embed = self.embedding(hyp_s)
+        u_encode = self.encode(u_embed, pre_l)
+        v_encode = self.encode(v_embed, hyp_l)
+        features = self.concat_embed(u_encode, v_encode)
+        out = self.clf(features)
+        return out
+
+    def concat_embed(self, u,v):
+        concat = torch.cat((u, v, (u-v).abs(), u*v), dim=1)
+        return concat
+
+    def encode(self, s, sl):
+        emb = self.encoder(s, sl)
+        return emb
