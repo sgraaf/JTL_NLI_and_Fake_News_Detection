8d4615bc56c307f06e62e3f717a42d8e3082b168
sgraaf <sgraaf@users.noreply.github.com>
2019-05-13 14:42:31 +0200

    Dataset cleaning

--- src/preprocess_fakenewsnet.py
+++ src/preprocess_fakenewsnet.py
@@ -8,9 +8,11 @@ it just dumps the collection of JSON files into train/val/test splits
 
 import csv
 import json
+import re
 from pathlib import Path
-from sklearn.model_selection import train_test_split
+
 from pandas import DataFrame as df
+from sklearn.model_selection import train_test_split
 
 
 def create_splits(df, ratio):
@@ -21,8 +23,62 @@ def create_splits(df, ratio):
     return train, test, val
 
 
+def is_valid_article(title, text, ILLEGAL_PATTERNS):
+    # check the title
+    for illegal_pattern in ILLEGAL_PATTERNS['title']:
+        if bool(re.search(illegal_pattern, title)):
+            return False
+    for illegal_pattern in ILLEGAL_PATTERNS['text']:
+        if bool(re.search(illegal_pattern, text)):
+            return False
+
+    return True
+
 DATA_DIR = Path.cwd().parent / 'fakenewsnet_dataset'
 JSON_FILES = sorted(DATA_DIR.rglob('*.json'))
+ILLEGAL_PATTERNS = {
+    'title': [
+        'X17 Online',
+        'Robot Check',
+        'Entertainment, News, and Lifestyle for Moms',
+        'NPR Choice page',
+        'Consent Form',
+        'LexisNexis(R) Publisher',
+        'FindArticles.com',
+        'CQ.com',
+        'Notice: Data not available: U.S. Bureau of Labor Statistics',
+        'Redirect Notice',
+        'Resource Not Available',
+        'Embed  Copy and paste this code into your blog to embed it on your site',
+        'The Colbert Report'
+    ],
+    'text': [
+        'Once registered, you can',
+        'U.S. CA U.K. AU Asia DE FR  E! Is Everywhere This content is available customized for our international audience',
+        'IMDb.com, Inc. takes no responsibility for the content or accuracy of the above',
+        'About Trendolizer™  Trendolizer™ (patent pending) automatically scans the internet for trending content',
+        'Please enable cookies on your web browser in order to continue.',
+        'About Your Privacy on this Site  Welcome!',
+        'version=""1.0"" encoding=""utf-8""?',
+        'We and our partners use cookies on this site to improve our service',
+        'This website uses profiling (non technical) cookies',
+        'Screen Rant – Privacy Policy  We respect your privacy and we are committed to safeguarding your privacy while online at our site',
+        'We use cookies to ensure that we give you the best experience on our website',
+        'Cookies help us deliver our Services',
+        'Netflix uses cookies for personalisation, to customise its online advertisements, and for other purposes',
+        'Wir verwenden Cookies, um Inhalte zu personalisieren',
+        'H&M använder cookies för att ge dig den bästa',
+        'We respect your privacy and we are committed to safeguarding your privacy while online at our site. The following discloses the information gathering and dissemination practices for this Web site.  This Privacy Policy',
+        'You must set your browser to accept cookies',
+        'We use cookies to enhance your experience, for analytics and',
+        'You are using an older browser version',
+        'The interactive transcript could not be loaded',
+        'Support the kind of journalism done by the NewsHour',
+        'Watch Queue Queue  Watch Queue Queue Remove',
+        'Interest Successfully Added  ',
+        "We're sorry  We are unable to locate information at"
+    ]
+}
 
 news_content = {
     'title': [],
@@ -42,9 +98,10 @@ for json_file in JSON_FILES:
         text = json_dict['text'].strip().replace('\n', ' ')
 
     if len(title) > 0 and len(text) > 0:
-        news_content['title'].append(title)
-        news_content['text'].append(text)
-        news_content['label'].append(label)
+        if is_valid_article(title, text, ILLEGAL_PATTERNS):
+            news_content['title'].append(title)
+            news_content['text'].append(text)
+            news_content['label'].append(label)
 
 df = df.from_dict(news_content)
 df_path = DATA_DIR / 'news_content.csv'
