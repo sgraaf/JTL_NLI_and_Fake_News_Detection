cdf1648345c4dcd3e64734d678b6238f8e136863
sgraaf <sgraaf@users.noreply.github.com>
2019-05-16 22:51:32 +0200

    Update dataset_example.py

--- src/dataset_example.py
+++ src/dataset_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-from torchtext.vocab import GloVe
 from pathlib import Path
-from dataset import FNNDataset
+from dataset import FNNDataset, SortPadBatch
 import torch.utils.data as data
 
 FNN_path = Path().cwd().parent / 'data' / 'FNN.pkl'
@@ -12,17 +12,24 @@ GloVe_vectors = GloVe()
 FNN = FNNDataset(FNN_path, GloVe_vectors)
 
 
-FNN_FL = data.DataLoader(
+FNN_DL = data.DataLoader(
     dataset=FNN,
-    batch_size=64,
+    batch_size=5,
     num_workers=1,
     shuffle=True,
-    drop_last=True
+    drop_last=True,
+    collate_fn=SortPadBatch()
 )
 
-for step, batch in enumerate(FNN_FL):
-    articles, labels = batch
-    print(articles.shape)
-    print(len(labels))
-    if step == 10:
+for step, batch in enumerate(FNN_DL):
+    articles, article_dims, labels = batch
+    for i in range(articles.shape[0]):
+        article = articles[i]
+        print(article.shape)
+        print(article_dims[i])
+        unpack = article[:article_dims[i][0], :article_dims[i][1], :]
+        print(unpack.shape)
+        print(unpack[:, :, 1])
+
+    if step == 1:
             break
