936c9a196cc3d7132cebd2e809bfe74ca9b69238
sgraaf <sgraaf@users.noreply.github.com>
2019-05-17 16:40:36 +0200

    Included simple model demo

--- src/dataset_example.py
+++ src/dataset_example.py
@@ -2,18 +2,34 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
+import torch
 import torch.utils.data as data
 from torchtext.vocab import GloVe
 
-from dataset import FNNDataset, SortPadBatch
+from dataset import FNNDataset, PadSortBatch
+from models import HierarchicalAttentionNet
+
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+    DEVICE = torch.device('cpu')
 
 FNN_path = Path().cwd().parent / 'data' / 'FNN.pkl'
 
 # load the GloVe vectors
 GloVe_vectors = GloVe()
 
+# load the FNN dataset
 FNN = FNNDataset(FNN_path, GloVe_vectors)
 
+# initialize the model
+model = HierarchicalAttentionNet(
+        input_dim=300,
+        hidden_dim=100,
+        num_classes=2,
+        embedding=None
+).to(DEVICE)
 
 FNN_DL = data.DataLoader(
     dataset=FNN,
@@ -21,18 +37,13 @@ FNN_DL = data.DataLoader(
     num_workers=1,
     shuffle=True,
     drop_last=True,
-    collate_fn=SortPadBatch()
+    collate_fn=PadSortBatch()
 )
 
 for step, batch in enumerate(FNN_DL):
     articles, article_dims, labels = batch
-    for i in range(articles.shape[0]):
-        article = articles[i]
-        print(article.shape)
-        print(article_dims[i])
-        unpack = article[:article_dims[i][0], :article_dims[i][1], :]
-        print(unpack.shape)
-        print(unpack[:, :, 1])
-
-    if step == 1:
-            break
+    
+    sent_embeds = model(articles, article_dims)
+    
+    if step == 10:
+        break
